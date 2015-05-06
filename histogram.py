3421208e79f9e60c75e638e06adb2f9f1603b258
Adam Derewecki <adam.derewecki@adroll.com>
2015-05-06 10:42:17 -0700

    Initial checkin

---
+++ histogram.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+import sys
+
+MAX_BAR_WIDTH = 80
+BIN_DIVISOR = 100
+
+if __name__ == '__main__':
+    print sys.argv
+    if len(sys.argv) > 1:
+        truncate_at = int(sys.argv[1])
+    else:
+        truncate_at = 100
+
+    events = sorted(int(x) for x in sys.stdin)
+    events_length = len(events)
+
+    bins = defaultdict(int)
+    keys = set()
+    for event in events:
+        key = event / BIN_DIVISOR
+        bins[key] += 1
+        keys.add(key)
+
+    sorted_keys = sorted(bins.keys())
+    print "{} items, {} keys".format(events_length, len(sorted_keys))
+
+    max_bar = float(max(bins.values()))
+    for i, timing_key in enumerate(sorted_keys):
+        if i > truncate_at: continue
+
+        bin_count = bins[timing_key]
+        width = int(bin_count / max_bar * MAX_BAR_WIDTH)
+        bucket_range = "{}-{}".format(i * BIN_DIVISOR, (i+1) * BIN_DIVISOR - 1)
+        print "{}\t{}\t{}".format(bucket_range, bin_count, '=' * width)
+
